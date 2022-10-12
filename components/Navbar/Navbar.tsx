55271ae3033a10e700bf48e8177ee241f68e3102
pawelLinek <pawellinek3D@gmail.com>
2022-10-12 19:35:16 +0200

    added new navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import { Menu, Notifications, Person } from "@material-ui/icons";
+import { useState } from "react";
 const Navbar = () => {
-    return ( 
-    <nav className='fixed w-full bg-stone-700'>
-        <ul className='flex justify-end gap-12 p-2 text-white mr-4'>
-            <li >
-                <Link href="/">
-                    <a>Główna</a>
-                </Link></li>
-            <li>Wyszukaj</li>
-        </ul>
-    </nav> );
-}
- 
+  const [clicked, Handle] = useState<boolean>(false);
+  console.log(clicked);
+  return (
+    <nav className="z-10 fixed bg-white w-full shadow shadow-grey text-black">
+      <ul className="flex gap-12 h-[9vh] mr-4 justify-between items-center">
+        <li className="p-5">
+          <a className="text-5xl">
+            <Menu fontSize="inherit" />
+          </a>
+        </li>
+        <li className="text-2xl  font-semibold absolute w-full text-center">
+          <Link href={"/"}>SZTIBLOG</Link>
+        </li>
+        <li>
+          <div className="flex gap-14 items-center">
+            <div className="text-4xl">
+              <Notifications fontSize="inherit" />
+            </div>
+            <div className="text-4xl">
+              <Person fontSize="inherit" />
+            </div>
+
+            <div
+              className="bg-white pl-6 pr-6 p-2 transition-all duration-150  rounded-xl border-2 border-gray-500 hover:border-gray-400 cursor-pointer"
+              onClick={() => Handle(!clicked)}
+            >
+              <button>Napisz</button>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 export default Navbar;
