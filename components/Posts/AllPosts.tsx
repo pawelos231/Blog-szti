406be519802f84b13c1bbad23953544c53cee4d1
pawelLinek <pawellinek3D@gmail.com>
2022-10-16 12:09:10 +0200

    addes static props to main page render

--- components/Posts/AllPosts.tsx
+++ components/Posts/AllPosts.tsx
@@ -5,18 +5,8 @@ import { useEffect, useState } from "react";
 import { SinglePostFromDatabase } from "../../interfaces/PostsInterface";
 import { CircularProgress } from "@material-ui/core";
 
-const AllPosts: () => JSX.Element = () => {
-  const [posts, SetPosts] = useState<Array<any>>([]);
-  const FetchPosts = async () => {
-    await fetch("/api/GetDataFromPost")
-      .then((res: Response) => res.json())
-      .then((data: Array<SinglePostFromDatabase>) => SetPosts(data));
-  };
-
-  useEffect(() => {
-    FetchPosts();
-  }, []);
-  if (posts.length == 0) {
+const AllPosts: ({ posts }) => JSX.Element = ({ posts }) => {
+  if (posts?.length == 0) {
     return (
       <div className="flex justify-center mt-12 text-black">
         <CircularProgress size={102} thickness={2.0} color="inherit" />
@@ -26,7 +16,7 @@ const AllPosts: () => JSX.Element = () => {
   return (
     <div className="w-full flex justify-center mt-10">
       <section className="flex flex-wrap w-[80%] gap-16 justify-center">
-        {posts.map((item: SinglePostFromDatabase, i: number) => {
+        {posts?.map((item: SinglePostFromDatabase, i: number) => {
           return (
             <Link href={`postsPage/${item._id}`}>
               <div
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
-let string: string = "siema" 
 import Header from "../components/Header/header";
 import AllPosts from "../components/Posts/AllPosts";
+import { GetStaticProps } from "next";
+import mongoose from "mongoose";
+const BlogPosts = require("../models/BlogPosts");
 
-export default function Home() {
-  return( 
-  <div className="flex justify-center w-full flex-col">
-    <Header/>
-    <AllPosts/>
-  </div>);
+export default function Home({ posts }) {
+  console.log(posts);
+  return (
+    <div className="flex justify-center w-full flex-col">
+      <Header />
+      <AllPosts posts={posts} />
+    </div>
+  );
 }
+export const getStaticProps: GetStaticProps = async () => {
+  await mongoose.connect(process.env.DATABASE_URL);
+  const data = await BlogPosts.find({});
+  const posts = JSON.parse(JSON.stringify(data));
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
