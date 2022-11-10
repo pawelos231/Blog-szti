dc0301bb569d4a94eeceecc39b84fad0b81ab49b
pawelLinek <pawellinek3D@gmail.com>
2022-11-10 13:02:09 +0100

    log

--- pages/api/user/fetchUserSpecificData.ts
+++ pages/api/user/fetchUserSpecificData.ts
@@ -4,9 +4,10 @@ const BlogPosts = require("../../../server/models/BlogPosts")
 import { VerifiedToken } from '../../../interfaces/Token'
 export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
     const token = String(req.headers["authorization"])
-    let decodedData: VerifiedToken = await verify(token, process.env.ACCESS_TOKEN_SECRET)
+    console.log(token)
+    const decodedData: VerifiedToken = await verify(token, process.env.ACCESS_TOKEN_SECRET)
+    console.log(decodedData)
     const data: any = await BlogPosts.find({UserEmail: decodedData.Email})
-    console.log(data)
     res.status(200).json({posts:{data}})
 
 }
