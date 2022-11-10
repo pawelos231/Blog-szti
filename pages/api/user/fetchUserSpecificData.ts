75ec763f4b125fa034eb581ae1383024cf4e1e90
pawelLinek <pawellinek3D@gmail.com>
2022-11-10 22:15:12 +0100

    mongoose connect added

--- pages/api/user/fetchUserSpecificData.ts
+++ pages/api/user/fetchUserSpecificData.ts
@@ -1,13 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import mongoose from 'mongoose'
 import {verify} from '../../../server/helpers/validateToken'
 const BlogPosts = require("../../../server/models/BlogPosts")
 import { VerifiedToken } from '../../../interfaces/Token'
 export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
+    await mongoose.connect(process.env.DATABASE_URL)
     const token = String(req.headers["authorization"])
     console.log(token)
     const decodedData: VerifiedToken = await verify(token, process.env.ACCESS_TOKEN_SECRET)
     console.log(decodedData)
-    const data: any = await BlogPosts.find({UserEmail: String(decodedData.Email).trim()})
+    const data: any = await BlogPosts.find({UserEmail: String(decodedData.Email)})
     console.log(data, "nie")
     if(data){
         res.status(200).json({posts:{data}})
@@ -15,6 +17,4 @@ export default async function Handler(req: NextApiRequest, res: NextApiResponse)
     else{
         res.status(400).json({text:"nie"})
     }
-  
-
 }
