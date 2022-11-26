74c27d40a04733108d58bdf5e99497ce808807c2
pawelLinek <pawellinek3D@gmail.com>
2022-11-26 15:29:05 +0100

    small route change

--- components/userDetails/CreatedPosts/CreatedPosts.tsx
+++ components/userDetails/CreatedPosts/CreatedPosts.tsx
@@ -14,7 +14,7 @@ const CreatedPosts = ({ createdPosts }) => {
           <div className="flex  w-[50%] justify-center flex-col gap-4 ">
             {createdPosts.map((item: SinglePostFromDatabase, i: number) => {
               return (
-                <Link href={`../postsPage/${item._id}`}>
+                <Link href={`../../postsPage/${item._id}`}>
                   <div
                     className="basis-[75%] p-4 rounded-sm flex min-h-[35vh]  border-y-[1.5px] transition-all duration-100 border-gray-300 relative hover:bg-gray-100"
                     key={i}
