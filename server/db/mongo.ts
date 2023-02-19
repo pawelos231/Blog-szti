8902021dd39f3adef5e3dd7171714ffa8d33f357
pawelLinek <pawellinek3D@gmail.com>
2023-02-19 16:03:31 +0100

    fixed connection to mongoose, cause i am stupid and tried connecting to mongo instance instead of mongoose

--- server/db/mongo.ts
+++ server/db/mongo.ts
@@ -8,16 +8,10 @@ if (!MONGODB_URI) {
   )
 }
 
-async function dbConnect (){
- 
-    if(mongoose.connections[0].readyState) {
-        return mongoose.connections[0]
-    } else {
-         return mongoose.connect(MONGODB_URI, ()=> {
+async function dbConnect (){ 
+    return mongoose.connect(MONGODB_URI, ()=> {
              console.log("your db is connected now")
-        })
-    }
-    
+    }) 
   }
 
 export default dbConnect
