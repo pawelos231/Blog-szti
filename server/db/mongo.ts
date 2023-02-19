c9a3a60d52e30fbbe4daef2cccb4dd8a3aa5b707
pawelLinek <pawellinek3D@gmail.com>
2023-02-19 15:48:56 +0100

    fixed connection to mongoose, cause i am stupid and tried connecting to mongo instance instead of mongoose

--- server/db/mongo.ts
+++ server/db/mongo.ts
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose'
 
-
-
 const MONGODB_URI = process.env.DATABASE_URL
 
 if (!MONGODB_URI) {
@@ -9,41 +7,16 @@ if (!MONGODB_URI) {
     'Please define the MONGODB_URI environment variable inside .env'
   )
 }
-const OPTIONS = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    bufferCommands: false,
-    bufferMaxEntries: 0,
-    useFindAndModify: true,
-    useCreateIndex: true
-  }
 
 async function dbConnect (): Promise<any> {
-    if(process.env.NODE_ENV !== "production") {
-        let cached: any = global.mongoose
-
-        if (!cached) {
-          cached = global.mongoose = { conn: null, promise: null }
-        }
-        if (cached.conn) {
-            return cached.conn
-          }
-        
-          if (!cached.promise) {
-            cached.promise = mongoose.connect(MONGODB_URI, OPTIONS).then((mongoose) => {
-              return mongoose
-            })
-          }
-          cached.conn = await cached.promise
-          return cached.conn
+ 
+    if(mongoose.connections[0].readyState) {
+        console.log("connected already")
+        return 
     } else {
-       if(mongoose.connections[0].readyState) {
-            console.log("connected already")
-       } else {
-            mongoose.connect(MONGODB_URI, OPTIONS, ()=> {
-                console.log("your db is connected now")
-            })
-       }
+         return mongoose.connect(MONGODB_URI, ()=> {
+             console.log("your db is connected now")
+        })
     }
     
   }
