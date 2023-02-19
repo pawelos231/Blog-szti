41d94c38b9bccb5cda0c8225a03f06071ac1a8e9
pawelLinek <pawellinek3D@gmail.com>
2023-02-19 15:03:22 +0100

    :|

--- pages/api/user/UserLikedPosts.ts
+++ pages/api/user/UserLikedPosts.ts
@@ -5,7 +5,7 @@ import { getLikedUserPosts } from "@server/db/posts";
 export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
     const token: string = String(req.headers["authorization"])
     const {Name}: VerifiedToken = await verify(token, process.env.ACCESS_TOKEN_SECRET)
-
+    console.log(Name)
     const {likedUserPosts, error}: any = await getLikedUserPosts(String(Name))
 
     res.status(200).json(likedUserPosts)
--- server/db/mongo.ts
+++ server/db/mongo.ts
@@ -1,20 +1,17 @@
 import { MongoClient } from 'mongodb'
+import mongoose from 'mongoose'
 const URI: string = process.env.DATABASE_URL
-const options = {}
 
 if (!URI) throw new Error("add DATABASE_URL to .env file")
 
-let client: MongoClient = new MongoClient(URI, options)
 let clientPromise: any
 
-if(process.env.NODE_ENV !== "production") {
-    if(!global._mongoClientPromise){
-        global._mongoClientPromise = client.connect()
-    }
-    clientPromise = global._mongoClientPromise
-} else {
-    clientPromise = client.connect()
+
+if(!global._mongoClientPromise){
+    global._mongoClientPromise = mongoose.connect(URI)
 }
+clientPromise = global._mongoClientPromise
+
 export default clientPromise
     
     
