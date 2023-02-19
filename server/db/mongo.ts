7ad1beb565a2c1f07dea42f9384949fceaddc000
pawelLinek <pawellinek3D@gmail.com>
2023-02-19 15:06:19 +0100

    :|

--- server/db/mongo.ts
+++ server/db/mongo.ts
@@ -1,17 +1,20 @@
 import { MongoClient } from 'mongodb'
 import mongoose from 'mongoose'
 const URI: string = process.env.DATABASE_URL
+const options = {}
 
 if (!URI) throw new Error("add DATABASE_URL to .env file")
 
 let clientPromise: any
 
-
-if(!global._mongoClientPromise){
-    global._mongoClientPromise = mongoose.connect(URI)
+if(process.env.NODE_ENV !== "production") {
+    if(!global._mongoClientPromise){
+        global._mongoClientPromise = mongoose.connect(URI)
+    }
+    clientPromise = global._mongoClientPromise
+} else {
+    clientPromise = mongoose.connect(URI)
 }
-clientPromise = global._mongoClientPromise
-
 export default clientPromise
     
     
