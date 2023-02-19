ef5a563f598aafabdb6dbc0e6a3b71e0090fbf0c
pawelLinek <pawellinek3D@gmail.com>
2023-02-19 14:52:14 +0100

    produkcja wybuchla XDDD

--- server/db/mongo.ts
+++ server/db/mongo.ts
@@ -7,8 +7,13 @@ if (!URI) throw new Error("add DATABASE_URL to .env file")
 let client: MongoClient = new MongoClient(URI, options)
 let clientPromise: any
 
-if(!global._mongoClientPromise){
-    global._mongoClientPromise = client.connect()
+if(process.env.NODE_ENV !== "production") {
+    if(!global._mongoClientPromise){
+        global._mongoClientPromise = client.connect()
+    }
+    clientPromise = global._mongoClientPromise
+} else {
+    clientPromise = client.connect()
 }
 clientPromise = global._mongoClientPromise
 
